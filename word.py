46b385b5b7c81170d799575255bb34aeb193c3f0
Yash <ypt7895@gmail.com>
2023-02-09 17:13:18 +0530

    created class for word

--- word.py
+++ word.py
@@ -1,5 +1,27 @@
-def wordValidation(entered):
-    return (len(entered)==5) and entered.isalpha()
+from helpers import printChar
+import time
 
-#def compareWords(entered, base):
+class Word:
+    def __init__(self):
+        self.word = 'apple'
+        self.charset = set(self.word)
 
+    def getWord(self):
+        return self.word
+
+    def compareWord(self,entered):
+        matched = True
+        entered = entered.lower()
+        for i in range(5):
+            if entered[i]==self.word[i]:
+                printChar(0,entered[i])
+            else:
+                matched = False
+                if entered[i] in self.charset:
+                    printChar(1,entered[i])
+                else:
+                    print(entered[i])
+
+            time.sleep(1.5)
+
+        return matched
