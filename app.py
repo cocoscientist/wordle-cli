4dffa92c8ece4e6fdeeaf608d6617060f4868c2c
Yash <ypt7895@gmail.com>
2023-02-09 17:13:39 +0530

    implemented base game

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from word import wordValidation
+from helpers import wordValidation
+from word import Word
 
 def mainloop():
     curWord = ''
     TRIES = 6
     WIN = False
+    baseWord = Word()
     while TRIES>0 and not WIN:
         print("Tries Remaining:",TRIES)
         curWord = input("Enter your Word: ")
         while not wordValidation(curWord):
             print("Word entered incorrectly")
             curWord = input("Enter Word Again: ")
-        
-        print(curWord)
+
+        WIN = baseWord.compareWord(curWord)
         TRIES -= 1
+        if WIN:
+            print("You win, Word is", curWord.lower(), "and you took", (6-TRIES), "tries")
+        else:
+            print("Try Again")
+    
+    if not WIN:
+        print("You Lose! ", "Word is ", baseWord.getWord())
 
 
 if __name__ == "__main__":
